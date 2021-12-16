fd27ef39d2c2968d3ada7cfdf4ed6a88d4ecb0aa
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-12-16 12:26:31 -0700

    feat(Constants): Agregar constantes

---
+++ app/src/main/java/com/ex/tvmaze/common/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.ex.tvmaze.common.utils
+
+object Constants {
+        const val BASE_URL = "http://api.tvmaze.com/"
+        const val END_POINT = "shows"
+        const val END_POINT_EPISODES = "/{:id}/episodes"
+}

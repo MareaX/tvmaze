480a35e9d2f9e8f20a1348e25279c1cde4843801
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-12-17 12:33:33 -0700

    feat(ShowInfoModel): Agregar codigo para la interaccion con las temporadas

--- app/src/main/java/com/ex/tvmaze/showInfoModule/viewModel/ShowInfoViewModel.kt
+++ app/src/main/java/com/ex/tvmaze/showInfoModule/viewModel/ShowInfoViewModel.kt
@@ -4,37 +4,31 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ex.tvmaze.common.entities.episodes.EpisodeEntity
+import com.ex.tvmaze.common.entities.season.SeasonEntity
 import com.ex.tvmaze.common.entities.shows.ShowEntity
 import com.ex.tvmaze.common.repository.ShowRepo
+import com.ex.tvmaze.mainModule.models.MainInteractor
 import com.ex.tvmaze.showInfoModule.model.ShowInfoInteractor
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class ShowInfoViewModel @Inject constructor(private val repo: ShowRepo) : ViewModel() {
 
-    private var show = ShowEntity()
-    internal val showSelected = MutableLiveData<ShowEntity>()
-
-    fun setStoreSelected(showEntity: ShowEntity){
-        showSelected.value = showEntity
+    private var idSeason = 0
+    var id: Int get() = idSeason
+    set(newId) {
+        idSeason = newId
     }
 
-    fun getStoreSelected():LiveData<ShowEntity>{
-        return  showSelected
-    }
+    private var interactor: ShowInfoInteractor = ShowInfoInteractor(this, repo)
+    private var _response = MutableLiveData<List<SeasonEntity>>()
 
+    val responseSeason: LiveData<List<SeasonEntity>> get() = _response
 
-    private var interactor: ShowInfoInteractor = ShowInfoInteractor()//(this, repo)
-    private var _response = MutableLiveData<List<EpisodeEntity>>()
-
-    val responseShow: LiveData<List<EpisodeEntity>> get() = _response
-
-    init {
-        loadEpisodes()
-    }
-
-    private fun loadEpisodes() {
-        /*interactor.setAllShows {
+    internal fun loadSeasons() {
+        interactor.setAllSeasons(id,{
             _response.postValue(it)
-        }*/
+        })
     }
 }

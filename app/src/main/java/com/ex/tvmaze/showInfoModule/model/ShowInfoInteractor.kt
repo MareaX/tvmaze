292d3999bddd66620813bb6425a0857e6c92a9b6
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-12-17 12:34:04 -0700

    fix(ShowInfoInteractor): correccion del codigo de interaccion de las temporadas

--- app/src/main/java/com/ex/tvmaze/showInfoModule/model/ShowInfoInteractor.kt
+++ app/src/main/java/com/ex/tvmaze/showInfoModule/model/ShowInfoInteractor.kt
@@ -1,4 +1,24 @@
 package com.ex.tvmaze.showInfoModule.model
 
-class ShowInfoInteractor {
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.ex.tvmaze.common.entities.season.SeasonEntity
+import com.ex.tvmaze.common.entities.shows.ShowEntity
+import com.ex.tvmaze.common.repository.ShowRepo
+import com.ex.tvmaze.mainModule.viewModel.MainViewModel
+import com.ex.tvmaze.showInfoModule.viewModel.ShowInfoViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class ShowInfoInteractor@Inject constructor(private val sivm: ShowInfoViewModel, private val repo: ShowRepo) {
+
+    internal fun setAllSeasons(id:Int, callback: (List<SeasonEntity>) -> Unit) = sivm.viewModelScope.launch {
+        repo.getSeasons(id).let { response ->
+            if (response.isSuccessful){
+                callback(response.body()!!)
+            } else{
+                Log.d("Tag","GetShows Error: ${response.code()}")
+            }
+        }
+    }
 }

5203f18a7d1e7e8b2810e75c5326e39b055b3c11
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-12-17 12:30:20 -0700

    fix(Constants): cambiar rutas del api

--- app/src/main/java/com/ex/tvmaze/common/utils/Constants.kt
+++ app/src/main/java/com/ex/tvmaze/common/utils/Constants.kt
@@ -3,5 +3,6 @@ package com.ex.tvmaze.common.utils
 object Constants {
         const val BASE_URL = "http://api.tvmaze.com/"
         const val END_POINT = "shows"
-        const val END_POINT_EPISODES = "/{:id}/episodes"
+        const val END_POINT_EPISODES = "/seasons/{id}/episodes"
+        const val END_POINT_SEASONS = "/{id}/seasons"
 }
